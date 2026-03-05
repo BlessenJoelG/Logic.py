33adfb812d8ff9b4694f04f74c08c4f39ddb05ee
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-03-05 09:55:53 +0530

    reset  dry file

--- Your_Dry_tools/dry_run.py
+++ Your_Dry_tools/dry_run.py
@@ -1,10 +1,1 @@
-words = ["cd","ac","dc","ca","zz"]
-seen = set()
-count = 0
-for word in words:
-    rev = word[::-1]
-    if rev in seen:
-        count += 1
-    else:
-        seen.add(word)
-print(count)
+pass
