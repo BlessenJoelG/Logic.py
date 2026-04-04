0af045f691bea9a5c59b19c553185914ea5d41de
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-05 01:00:45 +0530

    initial  q of cf

---
+++ LEETCODE-PRAC/2236. Root Equals Sum of Children.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        roots = root.val
+        root_left = root.left.val
+        root_right = root.right.val
+        if (roots == (root_left+root_right)):
+            return True
+        else:
+            return False
