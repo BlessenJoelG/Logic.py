123c64478dedae85a2b37adf48c3cccbb0386321
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-08 15:47:24 +0530

    refreshed dry file

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,15 +1,1 @@
-grid = [[9,1,7],[8,9,2],[3,4,6]]
-miss_val = [x for x in range(1,(len(grid)**2)+1)]
-repeat = []
-ans = []
-for x in grid:
-    repeat.extend(x)
-for y in repeat:
-    if repeat.count(y)>1:
-        ans.append(y)
-        break
-for z in miss_val:
-    if z not in repeat:
-        ans.append(z)
-        break
-print(ans)
+pass
