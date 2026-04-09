43270f22de7eed461fe2edb9e13a0c6511ee9af1
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-10 00:40:14 +0530

    initial q of lc

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,1 +1,8 @@
-pass
+n_k = list(map(int,input().split()))
+res = []
+for _ in range(1,n_k[0],2):
+        res.append(_)
+for _ in range(2,n_k[0],2):
+        res.append(_)
+print(res)
+print(res[n_k[1]-1])
