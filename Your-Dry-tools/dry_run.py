356fd99f226b3d99a5dc26968de940dc4cb8db0e
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-15 09:01:29 +0530

    cleared dry file

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,13 +1,1 @@
-m,c = 0,0
-for _ in range(int(input())):
-    m_c = list(map(int,input().split()))
-    if m_c[0]>m_c[1]:
-        m += 1
-    elif m_c[0]<m_c[1]:
-        c += 1
-if m > c:
-    print("Mishka")
-elif m < c:
-    print("Chris")
-elif m == c:
-    print("Friendship is magic!^^")
+pass
