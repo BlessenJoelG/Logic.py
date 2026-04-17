c13d197d5c6dc7dceff9f71d6c3f9bcf4bb9fc94
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-17 23:45:06 +0530

    cleared dry file

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,13 +1,1 @@
-for _ in range(int(input())):
-    n = int(input())
-    nums = list(map(int,input().split()))
-    max_idx = nums.index(max(nums))
-    if nums[0] != max(nums):
-        temp = nums[0]
-        nums[0] = max(nums)
-        nums[max_idx] = temp
-    c = 0
-    for k in range(1,n+1):
-        digi = nums[0:k]
-        c += max(digi)
-    print(c)
+pass
