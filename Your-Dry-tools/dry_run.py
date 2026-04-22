f35779b05d747671bce3e5edb6480dd24da673d7
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-22 23:44:02 +0530

    initial q of cf

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,1 +1,7 @@
-pass
+for _ in range(int(input())):
+    n_s_x = list(map(int,input().split()))
+    nums = list(map(int,input().split()))
+    if sum(nums) <= n_s_x[1] and (n_s_x[1]-sum(nums))%n_s_x[2] == 0:
+        print("YES")
+    else:
+        print("NO")
