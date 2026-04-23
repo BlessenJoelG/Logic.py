eb6442aa1426ef7c95a9aed161238a406533fa5a
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-23 21:53:33 +0530

    initial q of lc

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,7 +1,6 @@
-for _ in range(int(input())):
-    n_s_x = list(map(int,input().split()))
-    nums = list(map(int,input().split()))
-    if sum(nums) <= n_s_x[1] and (n_s_x[1]-sum(nums))%n_s_x[2] == 0:
-        print("YES")
-    else:
-        print("NO")
+nums = [0,1,2,3,4]
+index = [0,1,2,2,1]
+res = []
+for i in range(len(nums)):
+    res.insert(index[i],nums[i])
+print(res)
