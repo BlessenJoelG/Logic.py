32460e5e4151689b3ca6ad1cdda1b86690a7f57e
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-24 11:12:30 +0530

    git cleared dry file

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,10 +1,1 @@
-for _ in range(int(input())):
-    n = int(input())
-    if n == 2:
-        print(2)
-    elif n == 3:
-        print(3)
-    elif n%2 == 0:
-        print(0)
-    elif n%2 != 0:
-        print(1)
+pass
