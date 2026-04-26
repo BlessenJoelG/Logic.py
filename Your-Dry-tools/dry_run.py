94f25baca4c919e751ca9dafb3411bd48d49b2e9
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-26 22:52:01 +0530

    clean

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,8 +1,1 @@
-for _ in range(int(input())):
-    n = int(input())
-    cows = n // 4         
-    rem = n % 4          
-    if rem == 0:
-        print(cows)
-    else:
-        print(cows + 1)    
+pass
