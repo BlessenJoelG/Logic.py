e957f3a4cd16cbfd9b2359ac1e58edbc97187a4b
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-27 20:14:43 +0530

    initial q of lc

---
+++ LEETCODE-PRAC/2778. Sum of Squares of Special Elements .py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfSquares(self, nums: List[int]) -> int:
+        res = 0
+        for i in range(1,len(nums)+1):
+            if len(nums)%i == 0:
+                res += nums[i-1]**2
+        return res
+    
