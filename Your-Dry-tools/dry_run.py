13d2465566eff0621207d236f7877ac78ee9f57c
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-27 22:41:22 +0530

    cleared dry

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,8 +1,1 @@
-for i in range(int(input())):
-    a,b,c = map(int,input().split())
-    if abs(a-1)<abs(b-c)+abs(c-1):
-        print(1)
-    elif abs(b-c)+abs(c-1)<abs(a-1):
-        print(2)
-    else:
-        print(3)
+pass
