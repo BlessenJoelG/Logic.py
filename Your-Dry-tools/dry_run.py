e79d97601c07d9e09060d3fab350035f90d6563d
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-29 12:21:53 +0530

    cleared dry file

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,10 +1,1 @@
-n = "886996"
-# for i in range(len(n)):
-#     if i & 1 == 0:
-#         res.append(int(n[i]))
-#     else:
-#         res.append(-1*int(n[i]))
-# print(res)
-# print(sum(res))
-num = sum(int(n[i]) if i & 1 == 0 else -1*int(n[i]) for i in range(len(n)))
-print(num)
+pass
