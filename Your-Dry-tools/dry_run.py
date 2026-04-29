c3110ee81674e4abbb0428db6d022622374ca1d9
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-04-29 12:33:47 +0530

    cleared dry file

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,7 +1,1 @@
-for _ in range(int(input())):
-    res,n,ans = [],str(int(input())),0
-    for x in range(1,10):
-        res.extend([str(x)*i for i in range(1,5)])
-    for i in range(0,res.index(n)+1):
-        ans += len(res[i])
-print(ans)
+pass
