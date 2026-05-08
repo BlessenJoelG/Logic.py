3a515bd5c3e72fad65028012e831d3ab1418292d
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-05-08 22:36:29 +0530

    initIA Q OF LC

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,8 +1,1 @@
-for i in range(int(input())):
-    k,x = map(int,input().split())
-    for _ in range(1,k+1):
-        if (x-1)%3==0 and ((x-1)//3)%2==1:
-            x = (x-1)//3
-        else:
-            x = x*2
-print(x)
+pass
