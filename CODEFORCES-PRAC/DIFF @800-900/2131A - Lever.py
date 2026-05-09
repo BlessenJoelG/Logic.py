20d04bbbbc1148ce24a975d00722c811bc3aae92
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-05-09 22:26:31 +0530

    initial q of cf

---
+++ CODEFORCES-PRAC/DIFF @800-900/2131A - Lever.py
@@ -0,0 +1,6 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    print(sum(max(0, a[i] - b[i]) for i in range(n))+1)
