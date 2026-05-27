db797429a545fd3f9c8e87ae13fd21c2fa906142
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-05-27 12:15:43 +0530

    cleared dryfile

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,13 +1,1 @@
-events = ["W","W","W","W","W","W","W","W","W","W","W"]
-s,c = 0,0
-for i in events:
-    if c == 10:
-        break
-    else:
-        if i == "W":
-            c += 1
-        elif i == "WD" or i == "NB":
-            s += 1
-        else:
-            s += int(i)
-print([s,c])
+pass
