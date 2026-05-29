627529bd52c0223b88091ac248d70866cb114226
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-05-29 13:25:41 +0530

    initial q of lc

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,1 +1,10 @@
-pass
+nums = [96,89]
+mini = float("inf")
+for n in nums:
+    curr = 0
+    while(n>0):
+        d = n%10
+        curr += d
+        n = n//10
+    mini = min(mini,curr)
+print(mini)
