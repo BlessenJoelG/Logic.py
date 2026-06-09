96ef9b3abc1a53c52cea14dcd0f0c7e195ae2bc7
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-06-09 11:07:05 +0530

    initial q of lc

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,15 +1,4 @@
-nums = [-1,5,7,0]
-a,b = [],[]
-bool = False
-for i in range(len(nums)):
-    if i>1:
-        bool = True
-        for j in range(2,i):
-            if i%j == 0:
-                bool = False
-                break
-    if bool:
-        a.append(nums[i])
-    else:
-        b.append(nums[i])
-print(abs(sum(a)-sum(b)))
+nums = [1,3,2]
+k = 2
+ans = (max(nums)-min(nums))*k
+print(ans)
