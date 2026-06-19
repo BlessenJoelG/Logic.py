c649ef05e11855e69217d59eb9b809fe204b35d1
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-06-19 10:41:54 +0530

    leetcode problem of the day

--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,22 +1,5 @@
-key = "eljuxhpwnyrdgtqkviszcfmabo"
-message = "zwx hnfx lqantp mnoeius ycgk vcnjrdb"
-key = "".join(key.split(" "))
-ans = []
-for x in key:
-    if x not in ans:
-        ans.append(x)
-key = "".join(ans)
-freq = {}
-i,j = 97,0
-while(i<=122):
-    if key[j] not in freq:
-        freq[key[j]] = chr(i)
-    i += 1
-    j += 1
-res = ""
-for x in message:
-    if x == " ":
-        res = res + " "
-    else:
-        res = res + freq[x]
-print(res)
+gain = [-5,1,5,0,-7]
+ans = [0]
+for i in range(len(gain)):
+    ans.append(gain[i]+ans[i])
+print(max(ans))
