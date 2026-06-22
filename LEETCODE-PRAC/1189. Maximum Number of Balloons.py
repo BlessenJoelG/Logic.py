c111a1365c345f5505cb246f706fcdfc19386ec4
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-06-22 09:40:43 +0530

    lc challenge

---
+++ LEETCODE-PRAC/1189. Maximum Number of Balloons.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        freq,c = {},0
+        if (text.count("b")>=1 and text.count("a")>=1 and text.count("l")>=2 and text.count("o")>=2 and text.count("n")>=1):
+            for x in text:
+                if x not in freq:
+                    freq[x] = 1
+                else:
+                    freq[x] += 1
+            while(freq["b"]>=1 and freq["a"]>=1 and freq["l"]>=2 and freq["o"]>=2 and freq["n"]>=1):
+                freq["b"]-=1
+                freq["a"]-=1
+                freq["l"]-=2
+                freq["o"]-=2
+                freq["n"]-=1
+                c += 1
+        return c
--- Your-Dry-tools/dry_run.py
+++ Your-Dry-tools/dry_run.py
@@ -1,11 +1,16 @@
-costs = [1,3,2,4,1]
-coins = 7
-costs.sort()
-c = 0
-for price in costs:
-    if price<=coins:
-        coins -= price
+text =  "nlaebolko"
+freq,c = {},0
+if (text.count("b")>=1 and text.count("a")>=1 and text.count("l")>=2 and text.count("o")>=2 and text.count("n")>=1):
+    for x in text:
+        if x not in freq:
+            freq[x] = 1
+        else:
+            freq[x] += 1
+    while(freq["b"]>=1 and freq["a"]>=1 and freq["l"]>=2 and freq["o"]>=2 and freq["n"]>=1):
+        freq["b"]-=1
+        freq["a"]-=1
+        freq["l"]-=2
+        freq["o"]-=2
+        freq["n"]-=1
         c += 1
-    else:
-        break
-print(c)
+print(freq,c)
